ae136d27604b70595d620a06d8de4db8e4691553
unknown <duddjshfl@naver.com>
2022-09-01 16:15:57 +0900

    props 사용법

--- react-practice/src/Parent.js
+++ react-practice/src/Parent.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Child from './components/Child'
 
 function Parent(){
-    
+    const [Message, setMessage] = useState('')
 
     return (
         <div>
@@ -11,8 +11,13 @@ function Parent(){
             <span>Children in Parent Component</span>
             {/* Parent로부터 props를 받은 Child 컴포넌트 */}
             <Child message="hi" />
+            {/* Parnet로부터 props를 변수로 받은 Child 컴포넌트 */}
+            <Child message={Message} />
             {/* Parent로부터 props를 받지 않은 Child 컴포넌트 */}
             <Child />
+
+            {/* 버튼을 클릭하면 Message가 바뀌기 때문에 두번째 child 컴포넌트의 props도 변경된다 */}
+            <button onClick={() => setMessage('clicked')}>클릭</button>
         </div>
     )
 }
