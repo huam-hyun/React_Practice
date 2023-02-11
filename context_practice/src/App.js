e6a2fc516def0c0b4738198ecee50b5eca4170c3
박상현 <parksanghyun@bagsanghyeon-ui-noteubug.local>
2023-02-11 14:53:10 +0900

    3번 예제 추가

--- context_practice/src/App.js
+++ context_practice/src/App.js
@@ -1,5 +1,6 @@
 import Sample1 from './Sample1'
 import Sample2 from './Sample2';
+import Sample3 from './Sample3';
 
 function App() {
   return (
@@ -7,6 +8,9 @@ function App() {
       <Sample1 />
       <hr />
       <Sample2 />
+      <hr />
+      <Sample3 />
+      <hr />
     </div>
   );
 }
