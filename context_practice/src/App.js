00597697edf9ed2de68dc27f2d8637c17e869a07
박상현 <parksanghyun@bagsanghyeon-ui-noteubug.local>
2023-02-12 19:46:45 +0900

    Sample4 Update

--- context_practice/src/App.js
+++ context_practice/src/App.js
@@ -1,16 +1,22 @@
 import Sample1 from './Sample1'
 import Sample2 from './Sample2';
 import Sample3 from './Sample3';
+import Sample4 from './Sample4';
 
 function App() {
   return (
     <div className="App">
+      Sample1
       <Sample1 />
       <hr />
+      Sample2
       <Sample2 />
       <hr />
+      Sample3
       <Sample3 />
       <hr />
+      Sample4
+      <Sample4 />
     </div>
   );
 }
