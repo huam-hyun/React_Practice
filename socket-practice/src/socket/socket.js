d1d5b1afecec81ac79f98524a4872941c5810f3e
Sanghyun Park <sanghyun.park@ahha.ai>
2023-12-01 16:01:21 +0900

    feat: room 입장, 퇴장 함수 추가, 메시지 전송 일반 / room / dm 나눠서 보내도록 함수 수정

--- socket-practice/src/socket/socket.js
+++ socket-practice/src/socket/socket.js
@@ -1,22 +1,48 @@
 import { io } from "socket.io-client"
 
-const initClientSocket = () => {
-  const socket = io.connect('http://localhost:3001')
-
-  socket.on('connected', () => {
-    console.log('connected!')
-    socket.emit('message', { message: 'Hello world!' })
-  })
-
-  socket.on('receive_message', (data) => {
-    console.log('receive_message', data)
-  })
-
-  return socket
+const MESSAGE_TYPE = {
+  GLOBAL: 'global',
+  DIRECT_MESSAGE: 'dm',
+  ROOM: 'room',
 }
 
-export const socket = initClientSocket()
+export const socket = io.connect('http://localhost:3001')
+// 소켓 이벤트 정의
+socket.on('connected', () => {
+  console.log('connected!')
+  socket.emit('message', { message: 'Hello world!' })
+})
 
-export const sendSocketMessage = (message) => {
-  socket.emit('message', { message })
+socket.on('receive_message', (data) => {
+  console.log('receive_message', data)
+})
+// 소켓 사용하는 함수 정의
+export const sendSocketMessage = ({ type = MESSAGE_TYPE.GLOBAL, payload }) => {
+  // payload에 필요한 변수들은 알아서 추가할것
+  const { room, id, message } = payload
+  switch (type) {
+    case MESSAGE_TYPE.GLOBAL:
+      // 연결된 소켓으로 전송
+      socket.emit('message', { message })
+      break
+    case MESSAGE_TYPE.DIRECT_MESSAGE:
+      // 특정 소켓 id로 전송
+      socket.to(id).emit('message', { message })
+      break
+    case MESSAGE_TYPE.ROOM:
+      // 특정 room 으로 전송
+      socket.to(room).emit('message', { message })
+      break
+    default:
+  }
+}
+
+export const connectToRoom = (roomName) => {
+  socket.join(roomName)
+  socket.to(roomName).emit('message', { message: `socket ${socket.id} join room ${roomName}` })
+}
+
+export const leaveFromRoom = (roomName) => {
+  socket.to(roomName).emit('message', { message: `socket ${socket.id} leave room ${roomName}` })
+  socket.leave(roomName)
 }
